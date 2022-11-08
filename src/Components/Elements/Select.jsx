bb6c4e5b8f3f58907753a321eddf4d23426737e1
M Zubair <md.zubair143598@gmail.com>
2022-11-08 19:00:03 +0500

    updated

--- src/Components/Elements/Select.jsx
+++ src/Components/Elements/Select.jsx
@@ -1,24 +1,77 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Select = () => {
-
-    const location=useLocation();
-    const value=location.state;
+  const location = useLocation();
+  const value = location.state;
 
   return (
     <div className="Select-section">
-        <div className="container text-center">
-            <div className="row">
-                <div className="col-lg-12">
-                    <img src={value.img} className="" style={{width:"10%"}} alt="" />
-                    <h1 className='mt-3'>{value.text3}</h1>
-                    <p className='text-muted fw-bolder'>Deposit {value.text3} Tokens and Earn {value.text4}</p>
-                </div>
-            </div>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-lg-12">
+            <img src={value.img} className="" style={{ width: "10%" }} alt="" />
+            <h1 className="mt-3">{value.text3}</h1>
+            <p className="text-muted fw-bolder">
+              Deposit {value.text3} Tokens and Earn {value.text4}
+            </p>
+          </div>
         </div>
-    </div>
-  )
-}
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="card h-100 text-center">
+              <div className="card-body">
 
-export default Select
+                <div className="row">
+                  <div className="col-lg-6">
+                    <img src="./Assists/logo-cr.png" className="w-50" alt="" />
+                  </div>
+                  <div className="col-lg-6 mt-3 justify-content-center align-items-center d-flex">
+                    <h2 className="fw-bold" style={{color:"#0054cf"}}>{value.text4}</h2>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-lg-6">
+                    <p className="fw-bolder fs-5 text-muted">Earn</p>
+                    <p className="fw-bolder fs-5 text-muted">0.00</p>
+                  </div>
+                  <div className="col-lg-6">
+                  <button className="fs-5 rounded-2">Harvest</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-6">
+            <div className="card h-100 text-center">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-lg-6">
+                    <img src="./Assists/GMT94.png" className="w-50" alt="" />
+                  </div>
+                  <div className="col-lg-6 mt-3 justify-content-center align-items-center d-flex">
+                    <h2 className="fw-bold" style={{color:"#0054cf"}}>{value.text3}</h2>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-lg-6">
+                    <p className="fw-bolder fs-5 text-muted">Staked</p>
+                    <p className="fw-bolder fs-5 text-muted">0.00</p>
+                  </div>
+                  <div className="col-lg-6">
+                  <button className="fs-5 rounded-2">Harvest</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Select;
--- src/Components/Elements/StakePool.jsx
+++ src/Components/Elements/StakePool.jsx
@@ -11,59 +11,59 @@ const data = [
     text4: "MRACE",
   },
   {
-    img: "./Assists/GMT94.png",
-    title: "APY:600%",
+    img: "https://farms.marathoncash.com/assets/coin/BUSD94.png",
+    title: "APY:3500%",
     text1: "Deposit",
     text2: "Earn",
-    text3: "GMT",
+    text3: "BUST",
     text4: "MRACE",
   },
   {
-    img: "./Assists/GMT94.png",
-    title: "APY:600%",
+    img: "https://farms.marathoncash.com/assets/coin/USDT94.png",
+    title: "APY:4000%",
     text1: "Deposit",
     text2: "Earn",
-    text3: "GMT",
+    text3: "USDT",
     text4: "MRACE",
   },
   {
-    img: "./Assists/GMT94.png",
-    title: "APY:600%",
+    img: "https://farms.marathoncash.com/assets/coin/USDC94.png",
+    title: "APY:64000%",
     text1: "Deposit",
     text2: "Earn",
-    text3: "GMT",
+    text3: "USDC",
     text4: "MRACE",
   },
   {
-    img: "./Assists/GMT94.png",
-    title: "APY:600%",
+    img: "https://farms.marathoncash.com/assets/coin/WBNB94.png",
+    title: "APY:3400%",
     text1: "Deposit",
     text2: "Earn",
-    text3: "GMT",
+    text3: "WBNB",
     text4: "MRACE",
   },
   {
-    img: "./Assists/GMT94.png",
-    title: "APY:600%",
+    img: "https://farms.marathoncash.com/assets/coin/CAKE94.png",
+    title: "APY:4500%",
     text1: "Deposit",
     text2: "Earn",
-    text3: "GMT",
+    text3: "Cake",
     text4: "MRACE",
   },
   {
-    img: "./Assists/GMT94.png",
-    title: "APY:600%",
+    img: "https://farms.marathoncash.com/assets/coin/ETH94.png",
+    title: "APY:7800%",
     text1: "Deposit",
     text2: "Earn",
-    text3: "GMT",
+    text3: "ETH",
     text4: "MRACE",
   },
   {
-    img: "./Assists/GMT94.png",
-    title: "APY:600%",
+    img: "https://farms.marathoncash.com/assets/coin/1INCH94.png",
+    title: "APY:3400%",
     text1: "Deposit",
     text2: "Earn",
-    text3: "GMT",
+    text3: "1INCH",
     text4: "MRACE",
   },
 ];
@@ -97,12 +97,12 @@ const StakePool = () => {
                       </h3>
                       <div className="row">
                         <div className="col-6">
-                          <p className="fw-bolder fs-5  text-muted">Earn</p>
-                          <p className="fw-bolder fs-5  text-muted">DEPOSIT</p>
+                          <p className="fw-bolder fs-5  text-muted">{v.text1}</p>
+                          <p className="fw-bolder fs-5  text-muted">{v.text2}</p>
                         </div>
                         <div className="col-6">
-                          <p className="fw-bolder fs-5  text-dark">GMT</p>
-                          <p className="fw-bolder fs-5  text-dark">MRACE</p>
+                          <p className="fw-bolder fs-5  text-dark">{v.text3}</p>
+                          <p className="fw-bolder fs-5  text-dark">{v.text4}</p>
                         </div>
                       </div>
                       <hr />
